9ba87a78f6566dc2e8e981d132d4c90b2d283690
samfromaway@gmail.com <email@example.com>
2020-05-07 08:31:37 -0500

    error test

--- src/assets/scripts/components/QuizForm.js
+++ src/assets/scripts/components/QuizForm.js
@@ -19,7 +19,7 @@ const ContactForm = ({ questions }) => {
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'contact', ...name, ...email, ...message }),
+      body: encode({ 'form-name': 'contact', name, email, message }),
     })
       .then(() => alert('Success!'))
       .catch((error) => alert(error));
